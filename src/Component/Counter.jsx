b916061b3013bf5ce368a16b56ca95bce6c25a50
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-04-04 00:03:26 +0530

    create a first component

--- src/Component/Counter.jsx
+++ src/Component/Counter.jsx
@@ -1,10 +1,30 @@
-function Counter(){
-  return(
-<>
-<h1 >Counter</h1>
+import { useState } from "react";
 
-</>
-  )
+function Counter() {
+
+  let [counter, setCounter] = useState(0);
+
+  // Add function
+  const addValue = () => {
+    setCounter(counter + 1); 
+    // React ko bol rahe ho: counter update karo
+  };
+
+  // Remove function
+  const removeValue = () => {
+    setCounter(counter - 1);
+  };
+
+  return (
+    <>
+      <h1>Counter Value : {counter}</h1>
+
+      <button onClick={addValue}>Add Value</button>
+      <br />
+
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  );
 }
 
 export default Counter;

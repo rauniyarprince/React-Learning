331c5ff7e46ca3af7cee20db1f61f35ad2dc6549
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-04-08 21:14:09 +0530

    learn how to use plain css in our react

---
+++ Css-Uses/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Button from './Components/button/Button'
+import { Header } from './Components/Header/Header'
+
+
+function App() {
+
+  return (
+    <>
+    <h1>Uses of CSS</h1>
+    <Header/>
+    <Button/>
+  
+    </>
+  )
+}
+
+export default App
---
+++ Css-Uses/src/Components/button/Button.jsx
@@ -0,0 +1,10 @@
+import styles from  './Button.module.css'
+
+export default function Button() {
+  return (
+    <div>
+      <button className={styles.btn}>Click Me
+    </button></div>
+
+  )
+}

66a97409698ce8837a9b56789b4e5dc753b93623
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-05-04 22:02:20 +0530

    update code

--- Practise/src/App.jsx
+++ Practise/src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Greeting from './Greeting'
 
 const App = () => {
   return (
+    <>
+    <Greeting/>
     <h1>Hello react </h1>
+    </>
   )
 }
 export default App
---
+++ Practise/src/Greeting.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Greeting = () => {
+  return (
+    <div>
+<h1>Hello</h1>
+<h3>My Name is Prince</h3>
+
+
+
+
+    </div>
+  )
+}
+
+export default Greeting

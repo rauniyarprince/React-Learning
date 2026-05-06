7ac88d2ade13101814cfb42af056eddd9905586e
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-05-06 22:25:21 +0530

    practise props

--- Practise/src/App.jsx
+++ Practise/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Greeting from './Greeting'
+import Datapassing from './datapassing'
 
 const App = () => {
   return (
     <>
     <Greeting/>
+    <Datapassing user = "prince"/>
+    <Datapassing user2 = "Kartik"/>
+    <Datapassing user3 = "Pranshu"/>
     <h1>Hello react </h1>
     </>
   )
---
+++ Practise/src/Datapassing.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Datapassing = ({user,user1,user2}) => {
+  return (
+    <div>
+<h1>Hello{user} </h1>
+<h1>Hello{user2} </h1>
+<h1>Hello{user3} </h1>
+
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Datapassing

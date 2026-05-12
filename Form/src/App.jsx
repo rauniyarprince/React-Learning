d797a684dbf2b890a89a0980e9ba7088d341fdcc
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-05-12 22:04:48 +0530

    create Form

--- Form/src/App.jsx
+++ Form/src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import Form from '../../Form-handle/src/Form'
+import Form2 from './Form2'
 
 function App() {
 
   return (
     <>
     <Form/>
+    <Form2/>
     </>
   )
 }

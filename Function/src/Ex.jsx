bee38fd8ffa8d3bd3d2bbded17f46e57fbc63d55
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-05-17 22:25:15 +0530

    practise code

---
+++ Function/src/Ex.jsx
@@ -0,0 +1,14 @@
+function App() {
+  const name = "Prince";
+  const age = 20;
+
+  return (
+    <div>
+      <h1>Hello, {name}!</h1>
+      <p>Age: {age}</p>
+      <p>2 + 3 = {2 + 3}</p>
+    </div>
+  );
+}
+
+export default App;

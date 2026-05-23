4f32ab9d950ec7fbc6e7489f7d23f22f613b7c6d
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-05-23 19:16:39 +0530

    update the code

---
+++ Linking/src/Nav/Visit.jsx
@@ -0,0 +1,13 @@
+function Visiting(){
+    return(
+        <>
+        <h1>
+            hello myself prince rauniyar
+        </h1>
+        <p>-+
+i am from siswa bazar ,i cruntily pursuing BCA at BBDU
+        </p>
+        </>
+    )
+}
+export default Visiting;

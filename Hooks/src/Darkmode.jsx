8eba338a1e73d7e55f298942c8ff5a4d46fa08b3
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-05-28 22:08:40 +0530

    darkmode colour add

---
+++ Hooks/src/Darkmode.jsx
@@ -0,0 +1,23 @@
+// DarkMode.jsx
+import { useState } from "react";
+
+export default function DarkMode() {
+  const [dark, setDark] = useState(false);
+
+  return (
+    <div
+      style={{
+        background: dark ? "black" : "white",
+        color: dark ? "white" : "black",
+        height: "100vh",
+        padding: "20px",
+      }}
+    >
+      <h1>Dark Mode</h1>
+
+      <button onClick={() => setDark(!dark)}>
+        Change Theme
+      </button>
+    </div>
+  );
+}

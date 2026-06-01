533f800ab0c174b94cf622532880e6f74d322857
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-06-01 22:18:04 +0530

    usercard

---
+++ Practise/src/UserCard.jsx
@@ -0,0 +1,13 @@
+function UserCard() {
+  const name = "Prince";
+  const age = 20;
+
+  return (
+    <div>
+      <h2>Name: {name}</h2>
+      <p>Age: {age}</p>
+    </div>
+  );
+}
+
+export default UserCard;

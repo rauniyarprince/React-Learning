65974a89f3ce83eddb7e7cb28ac9ad7207470420
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-06-04 22:29:36 +0530

    login page

---
+++ Practise/src/LoginForm.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Email: ${email}`);
+  };
+
+  return (
+    <div>
+      <h2>Login Form</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <br /><br />
+
+        <input
+          type="password"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <br /><br />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;

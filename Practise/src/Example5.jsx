4d48fa4d0c79dbce32a849c311e3d04504334c8f
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-06-08 20:52:11 +0530

    update code

---
+++ Practise/src/Example5.jsx
@@ -0,0 +1,21 @@
+
+import React, { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h2>Counter App</h2>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+      <button onClick={() => setCount(count - 1)}>
+        Decrement
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
